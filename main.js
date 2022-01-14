e1611f215ddfd1428388b91142df057be9b51ee4
swastiodak <82050829+swastiodak@users.noreply.github.com>
2022-01-14 19:42:50 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas=createCanvas(250, 250);
+    canvas.center()
+}
+
+function draw(){
+
+}
+
+function takeSnapshot(){
+    save("glasses_image.png");
+}
